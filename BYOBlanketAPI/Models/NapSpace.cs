76109b27ea31abee8fda1ef04b4ef8d303dc8a58
Jesse <jesse.page@me.com>
2018-03-01 15:56:02 -0600

    the real initial commit, not lies like the other one

---
+++ BYOBlanketAPI/Models/NapSpace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BYOBlanketAPI.Models
+{
+    public class NapSpace
+    {
+        [Key]
+        public int NapSpaceId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [Required]
+        public string Rules { get; set; }
+        [Required]
+        public string Payment { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string PictureURL { get; set; }
+        [Required]
+        public ApplicationUser User {get; set;}
+        [Required]
+        public ApplicationUser UserEmail {get; set;}
+
+    }
+}
---
+++ BYOBlanketAPI/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BYOBlanketAPI.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationId { get; set; }
+        [Required]
+        public string NapSpaceTitle { get; set; }
+        [Required]
+        public string CalendarColor { get; set; }
+        [Required]
+        public DateTime StartDateTime { get; set; }
+        [Required]
+        public DateTime EndDateTime { get; set; }
+
+        [Required]
+        public int NapSpaceId { get; set; }
+        public NapSpace NapSpace { get; set; }
+
+        [Required]
+        public ApplicationUser UserId {get; set;}
+        public ApplicationUser UserEmail {get; set;}
+    }
+}
