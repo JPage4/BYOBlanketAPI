32396b5029382b7a93a3098807be93a641e60fc6
Jesse <jesse.page@me.com>
2018-03-05 15:19:52 -0600

    these controllers are gonna be so great and work the first time!

--- BYOBlanketAPI/Controllers/NapSpaceController.cs
+++ BYOBlanketAPI/Controllers/NapSpaceController.cs
@@ -2,45 +2,166 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BYOBlanketAPI.Data;
+using BYOBlanketAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BYOBlanketAPI.Controllers
 {
     [Route("api/[controller]")]
     public class NapSpaceController : Controller
     {
-        // GET: api/<controller>
+        private BYOBDbContext _context;
+        private ILogger _log;
+
+        public NapSpaceController(BYOBDbContext ctx, ILogger<NapSpaceController> logger)
+        {
+            _context = ctx;
+            _log = logger;
+        }
+
+        //Get all product types
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            //
+            var NapSpace = _context.NapSpace.ToList();
+            if (NapSpace == null)
+            {
+                return NotFound();
+            }
+            return Ok(NapSpace);
         }
 
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET a single product type
+        [HttpGet("{id}", Name = "GetSingleNapSpace")]
+
+        public IActionResult Get(int id)
         {
-            return "value";
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                NapSpace napSpace = _context.NapSpace.Single(n => n.NapSpaceId == id);
+
+                if (napSpace == null)
+                {
+                    return NotFound();
+                }
+                return Ok(napSpace);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound(ex);
+            }
         }
 
-        // POST api/<controller>
+/* Sample POST request:
+{
+    "Title": "Real Big Bed",
+    "Description": "You sleep in it.",
+    "Price": "$10",
+    "Rules": "Don't be weird",
+    "Payment": "MasterCard",
+    "Address": "444 Whatever St.",
+    "PictureURL": "pic.com",
+}
+*/
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody] NapSpace newNapSpace)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.NapSpace.Add(newNapSpace);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (NapSpaceExists(newNapSpace.NapSpaceId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetSingleNapSpacet", new { id = newNapSpace.NapSpaceId }, newNapSpace);
         }
 
-        // PUT api/<controller>/5
+        private bool NapSpaceExists(int NapSpaceId)
+        {
+            return _context.NapSpace.Any(g => g.NapSpaceId == NapSpaceId);
+        }
+
+        // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult PUT(int id, [FromBody] NapSpace newNapSpace)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != newNapSpace.NapSpaceId)
+            {
+                return BadRequest();
+            }
+
+            _context.NapSpace.Update(newNapSpace);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NapSpaceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
 
-        // DELETE api/<controller>/5
+        // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DELETE(int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            NapSpace napSpace = _context.NapSpace.Single(m => m.NapSpaceId == id);
+
+            if (napSpace == null)
+            {
+                return NotFound();
+            }
+
+            _context.NapSpace.Remove(napSpace);
+            _context.SaveChanges();
+
+            return Ok(napSpace);
+
         }
     }
 }
--- BYOBlanketAPI/Controllers/ReservationController.cs
+++ BYOBlanketAPI/Controllers/ReservationController.cs
@@ -2,45 +2,164 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BYOBlanketAPI.Data;
+using BYOBlanketAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BYOBlanketAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ReservationController : Controller
     {
-        // GET: api/<controller>
+        private BYOBDbContext _context;
+        private ILogger _log;
+
+        public ReservationController(BYOBDbContext ctx, ILogger<ReservationController> logger)
+        {
+            _context = ctx;
+            _log = logger;
+        }
+
+        //Get all product types
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            //
+            var Reservation = _context.Reservation.ToList();
+            if (Reservation == null)
+            {
+                return NotFound();
+            }
+            return Ok(Reservation);
         }
 
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET a single product type
+        [HttpGet("{id}", Name = "GetSingleReservation")]
+
+        public IActionResult Get(int id)
         {
-            return "value";
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Reservation reservation = _context.Reservation.Single(n => n.ReservationId == id);
+
+                if (reservation == null)
+                {
+                    return NotFound();
+                }
+                return Ok(reservation);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound(ex);
+            }
         }
 
-        // POST api/<controller>
+/* Sample POST request:
+{
+    "NapSpaceTitle": "Real Big Bed",
+    "CalendarColor": "#5f6dd0",
+    "StartDateTime": "new DateTime(2018, 3, 15, 12, 0, 0)",
+    "EndDateTime": "new DateTime(2018, 3, 15, 1, 0, 0)",
+    "NapSpaceId": ?,
+    "User": ?
+}
+*/
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody] Reservation newReservation)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Reservation.Add(newReservation);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReservationExists(newReservation.ReservationId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetSingleNapSpace", new { id = newReservation.ReservationId }, newReservation);
         }
 
-        // PUT api/<controller>/5
+        private bool ReservationExists(int ReservationId)
+        {
+            return _context.Reservation.Any(g => g.ReservationId == ReservationId);
+        }
+
+        // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult PUT(int id, [FromBody] Reservation newReservation)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != newReservation.ReservationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Reservation.Update(newReservation);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
 
-        // DELETE api/<controller>/5
+        // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DELETE(int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Reservation reservation = _context.Reservation.Single(m => m.ReservationId == id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservation.Remove(reservation);
+            _context.SaveChanges();
+
+            return Ok(reservation);
+
         }
     }
 }
