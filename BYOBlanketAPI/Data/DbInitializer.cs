345109ff1a35fd63270ee22d7244232236e04c9f
Jesse <jesse.page@me.com>
2018-03-06 12:56:02 -0600

    alas, my suffering continues

--- BYOBlanketAPI/Data/DbInitializer.cs
+++ BYOBlanketAPI/Data/DbInitializer.cs
@@ -19,6 +19,10 @@ namespace BYOBlanketAPI.Data
             
                 var roleStore = new RoleStore<IdentityRole>(context);
                 var userstore = new UserStore<User>(context);
+
+                if (!context.User.Any(u => u.FirstName == "Jesse"))
+                {
+                //  This method will be called after migrating to the latest version.
                 User user = new User
                 {
                     FirstName = "Jesse",
@@ -31,32 +35,9 @@ namespace BYOBlanketAPI.Data
                     LockoutEnabled = false,
                     SecurityStamp = Guid.NewGuid().ToString("D")
                 };
-
-                //if (!context.Roles.Any(r => r.Name == "Administrator"))
-                //{
-                //    var role = new IdentityRole { Name = "Administrator", NormalizedName = "Administrator" };
-                //    await roleStore.CreateAsync(role);
-                //}
-
-                if (!context.User.Any(u => u.FirstName == "Jesse"))
-                {
-                    //  This method will be called after migrating to the latest version.
-                    //User user = new User
-                    //{
-                    //    FirstName = "Jesse",
-                    //    LastName = "Page",
-                    //    UserName = "jesse@buttz.com",
-                    //    NormalizedUserName = "JESSE@BUTTZ.COM",
-                    //    Email = "jesse@buttz.com",
-                    //    NormalizedEmail = "JESSE@BUTTZ.COM",
-                    //    EmailConfirmed = true,
-                    //    LockoutEnabled = false,
-                    //    SecurityStamp = Guid.NewGuid().ToString("D")
-                    //};
-                    var passwordHash = new PasswordHasher<User>();
+                var passwordHash = new PasswordHasher<User>();
                     user.PasswordHash = passwordHash.HashPassword(user, "jjjjjj");
                     await userstore.CreateAsync(user);
-                    await userstore.AddToRoleAsync(user, "Administrator");
                 }
 
                 // Look for any products.
@@ -75,8 +56,8 @@ namespace BYOBlanketAPI.Data
                         Payment = "PayPal",
                         Address = "123 Buttz Lane",
                         PictureURL = "pic.com"
-                        //OwnerId = 1,
-                        //OwnerEmail = "jesse@buttz.com"
+                        UserId = context.User.Single(n => n.Email == "jesse@buttz.com").UserId,
+                        UserEmail = context.User.Single(n => n.Email == "jesse@buttz.com").Email
                     },
                     new NapSpace {
                         Title = "Tent",
@@ -85,9 +66,9 @@ namespace BYOBlanketAPI.Data
                         Rules = "The nicest tent of all time. It's for glamping.",
                         Payment = "Cash Money",
                         Address = "1111 Yep Rd",
-                        PictureURL = "pic.com"
-                        //OwnerId = 1,
-                        //OwnerEmail = "jesse@buttz.com"
+                        PictureURL = "pic.com",
+                        UserId = context.User.Single(n => n.Email == "jesse@buttz.com").UserId,
+                        UserEmail = context.User.Single(n => n.Email == "jesse@buttz.com").Email
                     }
                 };
 
@@ -104,8 +85,9 @@ namespace BYOBlanketAPI.Data
                         CalendarColor = "#5f6dd0",
                         StartDateTime = new DateTime(2018, 3, 15, 12, 0, 0),
                         EndDateTime = new DateTime(2018, 3, 15, 1, 0, 0),
-                        NapSpaceId = 2,
-                        User = user
+                        NapSpaceId = context.NapSpace.Single(n => n.Title == "Tent").NapSpaceId,
+                        UserId = context.User.Single(n => n.Email == "jesse@buttz.com").UserId,
+                        UserEmail = context.User.Single(n => n.Email == "jesse@buttz.com").Email
                     },
                    new Reservation
                    {
@@ -114,7 +96,8 @@ namespace BYOBlanketAPI.Data
                        StartDateTime = new DateTime(2018, 3, 15, 11, 0, 0),
                        EndDateTime = new DateTime(2018, 3, 15, 12, 0, 0),
                        NapSpaceId = context.NapSpace.Single(n => n.Title == "Big Comfy Couch").NapSpaceId,
-                       User = user
+                       UserId = context.User.Single(n => n.Email == "jesse@buttz.com").UserId,
+                       UserEmail = context.User.Single(n => n.Email == "jesse@buttz.com").Email
                    }
                 };
 
--- BYOBlanketAPI/Models/NapSpace.cs
+++ BYOBlanketAPI/Models/NapSpace.cs
@@ -27,6 +27,9 @@ namespace BYOBlanketAPI.Models
 
         [Required]
         public User User {get; set;}
-
+        [Required]
+        public int UserId {get; set;}
+        [Required]
+        public string UserEmail {get; set;}
     }
 }
